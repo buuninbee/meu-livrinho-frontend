dec843edadd52f498b39ce4681c3be023d7215da
buuninbee <buunin.bee@gmail.com>
2026-05-15 00:15:10 -0300

    feat: criação do componente de Footer

---
+++ src/modules/shared/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router";
+
+const Footer = () => {
+  return (
+    <footer class="bg-black text-white dark:bg-gray-900">
+      <div class="mx-auto max-w-7xl px-4 pt-16 pb-8 sm:px-6 lg:px-8 lg:pt-24">
+        <div class="text-center">
+          <h2 class="text-3xl font-extrabold sm:text-5xl dark:text-white">
+            Descubra um novo mundo com os livros
+          </h2>
+
+          <p class="mx-auto mt-4 max-w-sm text-gray-500 dark:text-gray-400">
+            Acesse o app e viva a experiência completa: cadastre livros ou adquira os seus favoritos
+          </p>
+
+          <NavLink
+            to="/cadastro"
+            class="mt-8 inline-block rounded-full border border-pink-500 px-12 py-3 text-sm font-medium text-white hover:bg-indigo-600 hover:text-white"
+          >
+            Fazer Cadastro
+          </NavLink>
+        </div>
+
+        <div class="mt-16 border-t border-gray-100 pt-8 sm:flex sm:items-center sm:justify-between lg:mt-24 dark:border-gray-800">
+          <ul class="flex flex-wrap justify-center gap-4 text-xs lg:justify-end">
+            <li>
+              <a href="#" class="text-gray-500 transition hover:opacity-75 dark:text-gray-400">
+                Termos e Condições
+              </a>
+            </li>
+
+            <li>
+              <a href="#" class="text-gray-500 transition hover:opacity-75 dark:text-gray-400">
+                Politica de privacidade
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
