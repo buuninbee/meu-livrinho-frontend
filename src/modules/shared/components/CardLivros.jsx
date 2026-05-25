5ba880bf2852c459b46f4a49224c409292b2d390
buuninbee <buunin.bee@gmail.com>
2026-05-24 23:22:32 -0300

    feat: atualizando o componente CardLivro para receber os dados da API

--- src/modules/shared/components/CardLivros.jsx
+++ src/modules/shared/components/CardLivros.jsx
@@ -2,16 +2,23 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/component
 import BadgeGenero from "./BadgeGenero";
 import { Star } from "lucide-react";
 import { NavLink } from "react-router";
+import ImagemPadrao from "@/assets/capaPadrao.png";
 
-function CardLivros({ nome, genero, autor, ano, descricao, avaliacao, capa }) {
+function CardLivros({ nome, genero, autor, ano, descricao, avaliacao, capa, id }) {
   return (
-    <div className="mx-auto w-full min-w-2xs max-w-sm bg-gray-50 border border-gray-300 rounded-lg">
+    <div className="w-full min-w-80  max-w-sm bg-gray-50 border border-gray-300 rounded-lg">
       <Collapsible className="rounded-md data-[state=open]:bg-muted">
         <CollapsibleTrigger asChild>
           <div className="grid gap-3 px-3 py-5">
-            <div className="grid gap-3">
-              <div className="w-auto ">
-                <img className="w-auto h-auto " src={capa} alt={nome} srcset="" />
+            <div className="grid gap-3 h-xl">
+              <div className="w-auto min-h-xl ">
+                <img
+                  className="w-auto min-h-125"
+                  height={300}
+                  src={capa ? capa : ImagemPadrao}
+                  alt={nome}
+                  srcset=""
+                />
               </div>
 
               <BadgeGenero genero={genero} />
@@ -34,7 +41,7 @@ function CardLivros({ nome, genero, autor, ano, descricao, avaliacao, capa }) {
           <p className="text-xl mb-3">{descricao}</p>
 
           <NavLink
-            to="/livro"
+            to={`/livro/${id}`}
             className="bg-pink-600 text-white text-lg text-center font-medium p-2 rounded-xl"
           >
             Ver mais{" "}
