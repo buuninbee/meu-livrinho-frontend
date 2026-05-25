93fff031936fd473fb8365c2dee6049e7fc6272a
buuninbee <buunin.bee@gmail.com>
2026-05-24 23:20:55 -0300

    feat: adicionando as rotas no arquivo main

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import LivroPagina from "./modules/shared/pages/LivroPagina.jsx";
 import Login from "@/pages/Login.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
+import PerfilResponsavel from "./modules/usuarioResponsavel/pages/PerfilResponsavel.jsx";
+import PerfilDependente from "./modules/usuarioResponsavel/pages/PerfilDependente.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<App />} />
-        <Route index path="/livro" element={<LivroPagina />} />
-        <Route index path="/login" element={<Login />} />
-        <Route index path="/cadastro" element={<Cadastro />} />
+        <Route path="/livro/:id" element={<LivroPagina />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
+        <Route path="/perfil-responsavel" element={<PerfilResponsavel />} />
+        <Route path="/perfil-dependente" element={<PerfilDependente />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
