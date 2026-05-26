00d57ac8ace4453d2a4d22d48608fe99555d18ec
buuninbee <buunin.bee@gmail.com>
2026-05-25 23:28:31 -0300

    refactor: adicionando os botões de cadastro e login no header

--- src/modules/usuarioResponsavel/components/NavBar.jsx
+++ src/modules/usuarioResponsavel/components/NavBar.jsx
@@ -1,15 +1,32 @@
 import { Search } from "lucide-react";
-import CarrinhoCompras from "@/modules/usuarioResponsavel/components/CarrinhoCompras";
+// import CarrinhoCompras from "@/modules/usuarioResponsavel/components/CarrinhoCompras";
 import logo from "@/assets/logo.svg";
+import { NavLink } from "react-router";
 
 const NavBar = () => {
   return (
-    <header className="bg-black grid gap-6 px-6 py-4 mb-6">
+    <header className="bg-black grid gap-6 px-6 py-4">
       <nav className="flex justify-between items-center">
         <div className="w-58">
           <img src={logo} alt="" />
         </div>
-        <CarrinhoCompras />
+        {/* <CarrinhoCompras /> */}
+
+        <div>
+          <NavLink
+            className=" text-white px-3 py-2 text-center font-medium p-2 rounded-xl"
+            to="/cadastro"
+          >
+            Cadastro
+          </NavLink>
+
+          <NavLink
+            className="bg-pink-600 text-white px-3 py-2 text-center font-medium p-2 rounded-xl"
+            to="/login"
+          >
+            Login
+          </NavLink>
+        </div>
       </nav>
 
       <div className="flex items-center px-3 py-1 bg-white rounded-xl">
