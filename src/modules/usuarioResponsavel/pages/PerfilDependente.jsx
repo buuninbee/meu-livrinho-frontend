8848d217c560382a73b562d12cf65b42222db7d8
buuninbee <buunin.bee@gmail.com>
2026-05-25 23:25:56 -0300

    refactor: adicionando o botão de voltar para a pagina anterior

--- src/modules/usuarioResponsavel/pages/PerfilDependente.jsx
+++ src/modules/usuarioResponsavel/pages/PerfilDependente.jsx
@@ -1,12 +1,15 @@
 import { ChevronLeft, PlusCircle, Trash } from "lucide-react";
 import DialogoEditarPerfilDependente from "../components/DialogoEditarPerfilDependente";
+import { NavLink } from "react-router";
 
 const PerfilDependente = () => {
   return (
     <>
       <header className="bg-gray-100">
         <nav className="grid grid-cols-4 py-5 px-4 items-center justify-between">
+          <NavLink to="/perfil-responsavel">
           <ChevronLeft className="" />
+          </NavLink>
           <h2 className="text-center col-span-3 text-2xl">Perfil dos Dependentes</h2>
         </nav>
       </header>
