dc23b206c7011649408ee83c715a55e2ed68a930
buuninbee <buunin.bee@gmail.com>
2026-05-25 23:22:12 -0300

    refactor: adicionando uma imagem de fundo no começo da pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,47 @@ import imagemDeFundoHome from "@/assets/imagem-de-fundo-home.png";
 function App() {
   const [livros, setLivros] = useState([]);
   const listaGenero = ["Fantasia", "Romance", "Terror", "Ficção", "Drama", "Aventura"];
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function carregarLivros() {
+      setLoading(true);
       const { data } = await ListaLivrosModel();
       await setLivros(data || []);
+      setLoading(false);
     }
-
     carregarLivros();
   }, []);
 
   return (
     <>
       <NavBar />
-      <main className="px-6">
+      <div className="pb-6">
+        <div className="grid">
+          <div className="absolute z-10 grid self-end justify-self-center px-3 pb-6 gap-2 sm:px-8 md:pb-12">
+            <h1 className="text-2xl font-medium font-sans text-center text-white sm:text-3xl lg:text-5xl">
+              Meu Livrinho: O Porto Seguro da Leitura Digital
+            </h1>
+            <p className="text-white font-serif text-xs text-center italic sm:text-lg lg:text-2xl">
+              Uma plataforma focada em controle parental passivo que ajuda na formação de pequenos
+              leitores, subtituindo a hiperestimulação das telas por um tempo de qualidade e
+              desenvolvimento
+            </p>
+          </div>
+
+          <img
+            width="640"
+            height="360"
+            className="object-cover z-0 w-full lg:h-175 lg:object-fill"
+            src={imagemDeFundoHome}
+            alt="Uma mãe lendo para sey filho"
+            title=""
+          />
+        </div>
+      </div>
+      <main className="px-6 sm:px-12">
         <section className="mb-6">
-          <ul className="flex gap-4 overflow-x-auto sm:fle">
+          <ul className="flex gap-4 overflow-x-auto sm:justify-center">
             {listaGenero.map((genero) => {
               return (
                 <li key={genero}>
