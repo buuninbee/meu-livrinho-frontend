f0c84bf6102f6128a83f6348f0b8104dc77ad8f4
buuninbee <buunin.bee@gmail.com>
2026-06-02 23:34:43 -0300

    refactor: adicionando os layouts nas rotas

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,22 @@ import Login from "@/pages/Login.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
 import PerfilResponsavel from "./modules/usuarioResponsavel/pages/PerfilResponsavel.jsx";
 import PerfilDependente from "./modules/usuarioResponsavel/pages/PerfilDependente.jsx";
+import Index from "./modules/usuarioResponsavel/layout/Index.jsx";
+import LayoutSemLogin from "./modules/shared/layout/Index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<App />} />
-        <Route path="/livro/:id" element={<LivroPagina />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/" element={<Index />}>
+          <Route index element={<App />} />
+          <Route path="/livro/:id" element={<LivroPagina />} />
+        </Route>
+
+        <Route element={<LayoutSemLogin />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Route>
 
         <Route path="/perfil-responsavel" element={<PerfilResponsavel />} />
         <Route path="/perfil-dependente" element={<PerfilDependente />} />
