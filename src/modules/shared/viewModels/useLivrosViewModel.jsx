51fbab8babf6acf2d4b3180777fe13745b7d0591
buuninbee <buunin.bee@gmail.com>
2026-06-02 23:08:28 -0300

    feat: adicionando hook para trazer um livro em especifico

---
+++ src/modules/shared/viewModels/useLivrosViewModel.jsx
@@ -0,0 +1,30 @@
+import { useParams } from "react-router";
+import { LivroModel } from "../models/LivroModel";
+import { useEffect, useState } from "react";
+
+const useLivrosViewModel = () => {
+  const { id } = useParams();
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const buscarLivro = async () => {
+      try {
+        const { data } = await LivroModel(id);
+        setData(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    buscarLivro();
+  }, [id]);
+
+  return { data, loading, error };
+};
+
+export default useLivrosViewModel;
