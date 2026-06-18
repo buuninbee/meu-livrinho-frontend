f225b31328dcf90d4d2d265067b4f17bfe71a159
buuninbee <buunin.bee@gmail.com>
2026-06-18 00:33:25 -0300

    refactor: alterando as rotas da aplicação e adicionando os layouts

--- src/main.jsx
+++ src/main.jsx
@@ -8,25 +8,36 @@ import Login from "@/pages/Login.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
 import PerfilResponsavel from "./modules/usuarioResponsavel/pages/PerfilResponsavel.jsx";
 import PerfilDependente from "./modules/usuarioResponsavel/pages/PerfilDependente.jsx";
-import Index from "./modules/usuarioResponsavel/layout/Index.jsx";
-import LayoutSemLogin from "./modules/shared/layout/Index.jsx";
+import LayoutNaoAutenticado from "./modules/shared/layout/LayoutNaoAutenticado.jsx";
+import SemFooter from "./modules/shared/layout/LayoutSemFooter.jsx";
+import AdicionarLivro from "./modules/usuarioResponsavel/pages/AdicionarLivro.jsx";
+import PaginaInicial from "./modules/usuarioResponsavel/pages/PaginaInicial.jsx";
+import LayoutAutenticado from "./modules/usuarioResponsavel/layout/LayoutAutenticado.jsx";
+import LivroAberto from "./modules/shared/pages/LivroAberto.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />}>
+        <Route path="/" element={<LayoutNaoAutenticado />}>
           <Route index element={<App />} />
-          <Route path="/livro/:id" element={<LivroPagina />} />
+          <Route path="/livro/:slug" element={<LivroPagina />} />
         </Route>
 
-        <Route element={<LayoutSemLogin />}>
+        <Route element={<SemFooter />}>
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
         </Route>
 
+        <Route element={<LayoutAutenticado />}>
+          <Route path="/pagina-inicial" element={<PaginaInicial />} />
+        </Route>
+
+        <Route path="/leitura/:slug" element={<LivroAberto />} />
+
         <Route path="/perfil-responsavel" element={<PerfilResponsavel />} />
         <Route path="/perfil-dependente" element={<PerfilDependente />} />
+        <Route path="/adicionar-livro" element={<AdicionarLivro />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
