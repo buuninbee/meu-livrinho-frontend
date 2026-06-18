1a3d132a54e2680ea5f45418ed758d7e878c6d90
buuninbee <buunin.bee@gmail.com>
2026-06-18 00:20:09 -0300

    refactor: adicionando as funções de adicionar logica de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,58 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import logo from "@/assets/logo.svg";
+import handleLogin from "@/modules/usuarioResponsavel/auth/useLogin";
+import { schemaLogin } from "@/modules/usuarioResponsavel/schemas/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast, Toaster } from "sonner";
 
-const login = () => {
+const Login = () => {
+  const navigate = useNavigate();
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(schemaLogin),
+    defaultValues: {
+      email: "",
+      senha: "",
+    },
+  });
+
+  const submitLogin = async ({ email, senha }) => {
+    const result = await handleLogin(email, senha);
+
+    if (!result) {
+      toast.error("Credenciais não encontradas. Verifique seu usuário e senha.");
+      reset();
+      return;
+    }
+
+    toast.success("Login realizado com sucesso! Bem-vindo de volta.");
+    localStorage.setItem("emailUsuario", email);
+
+    setTimeout(() => {
+      navigate("/pagina-inicial");
+      reset();
+    }, 1000);
+  };
+
   return (
     <>
+      <Toaster position="top-center" richColors />
       <main className="px-6 h-screen py-10 bg-blue-400">
-        <form className="mx-auto max-w-md space-y-4 rounded-lg border border-gray-300 bg-gray-100 p-6">
+        <form
+          onSubmit={handleSubmit(submitLogin)}
+          className="mx-auto max-w-md space-y-4 rounded-lg border border-gray-300 bg-gray-100 p-6"
+        >
           <div className="grid justify-center">
             <img className="w-56 text-center" src={logo} alt="" />
           </div>
-          <div>
+
+          <div className="grid">
             <label className="block text-sm font-medium text-gray-900" for="name">
               Email
             </label>
@@ -19,10 +62,12 @@ const login = () => {
               id="name"
               type="email"
               placeholder="email@gmail.com"
+              {...register("email")}
             />
+            {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
           </div>
 
-          <div>
+          <div className="grid">
             <label className="block text-sm  font-medium text-gray-900" for="email">
               Senha
             </label>
@@ -32,7 +77,9 @@ const login = () => {
               id="email"
               type="password"
               placeholder="sua senha"
+              {...register("senha")}
             />
+            {errors.senha && <p className="text-red-500 text-sm">{errors.senha.message}</p>}
           </div>
 
           <p className="text-sm">
@@ -42,16 +89,16 @@ const login = () => {
             </NavLink>
           </p>
 
-          <NavLink
-            to="/perfil-responsavel"
+          <button
+            disabled={isSubmitting ? "disabled" : ""}
             className="block w-full rounded-lg text-center border border-pink-600 bg-pink-600 px-12 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-pink-600"
           >
-            Entrar
-          </NavLink>
+            {isSubmitting ? "Carregando..." : "Entrar"}
+          </button>
         </form>
       </main>
     </>
   );
 };
 
-export default login;
+export default Login;
