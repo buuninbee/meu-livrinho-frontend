81f34ddf7186a5afb40968c98d64c4232475987e
buuninbee <buunin.bee@gmail.com>
2026-06-18 00:08:52 -0300

    feat: criando pagina onde os livros são lidos

---
+++ src/modules/shared/pages/LivroAberto.jsx
@@ -0,0 +1,31 @@
+import { useParams, useLocation, Link } from "react-router";
+
+import EpubVisualizador from "../components/EpubVisualizador";
+import PdfVizualizador from "../components/PdfVisualizador";
+import { ChevronLeft } from "lucide-react";
+
+const LivroAberto = () => {
+  const { slug } = useParams();
+
+  console.log(slug);
+  const locationState = useLocation();
+  const hasReader = locationState.state?.hasReader ?? true;
+
+  return (
+    <div className="flex w-auto z-50 h-dvh flex-col">
+      <div className="pt-10 bg-white grid px-6 gap-5">
+        <Link to="/perfil-responsavel" className="flex text-lg items-center gap-2 text-gray-500">
+          <ChevronLeft />
+          Voltar para Biblioteca
+        </Link>
+        <span className="text-2xl font-medium text-center">Lendo: {slug}</span>
+      </div>
+
+      <div className="flex-1 relative bg-pink-700">
+        {hasReader ? <EpubVisualizador /> : <PdfVizualizador />}
+      </div>
+    </div>
+  );
+};
+
+export default LivroAberto;
